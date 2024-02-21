4482a89d781ca292e42c849fc374caa24f391c86
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-02-20 22:43:09 -0300

    feat(ProtoChain): add default states

--- contracts/ProtoCoin.sol
+++ contracts/ProtoCoin.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract ProtoCoin {}
+contract ProtoCoin {
+    string public name = "ProtoCoin";
+    string public symbol = "PRC";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 100 * 10 ** decimals;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+}
--- test/ProtoCoin.test.ts
+++ test/ProtoCoin.test.ts
@@ -7,13 +7,32 @@ describe("ProtoCoin Tests", function () {
     const [owner, otherAccount] = await ethers.getSigners()
 
     const ProtoCoin = await ethers.getContractFactory("ProtoCoin")
-    const protocoin = await ProtoCoin.deploy()
+    const protoCoin = await ProtoCoin.deploy()
 
-    return { protocoin, owner, otherAccount }
+    return { protoCoin, owner, otherAccount }
   }
 
-  it("Should test", async function () {
-    const { protocoin, owner, otherAccount } = await loadFixture(deployFixture)
-    expect(true).to.equal(true)
+  it("Should have correct name", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture)
+    const name = await protoCoin.name()
+    expect(name).to.equal("ProtoCoin")
+  })
+
+  it("Should have correct symbol", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture)
+    const symbol = await protoCoin.symbol()
+    expect(symbol).to.equal("PRC")
+  })
+
+  it("Should have correct decimals", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture)
+    const decimals = await protoCoin.decimals()
+    expect(decimals).to.equal(18)
+  })
+
+  it("Should have correct totalSupply", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture)
+    const totalSupply = await protoCoin.totalSupply()
+    expect(totalSupply).to.equal(100n * 10n ** 18n)
   })
 })
