5bc4b123f08879dc8e75c853ec3cfa342f8fd7cf
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-02-23 05:58:52 -0300

    build: add deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,33 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 import "@nomicfoundation/hardhat-toolbox"
 import { HardhatUserConfig } from "hardhat/config"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: "0.8.24",
+  defaultNetwork: "local",
+  networks: {
+    local: {
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+    },
+    sepolia: {
+      url: process.env.INFURA_URL,
+      chainId: 11155111,
+      accounts: {
+        mnemonic: process.env.METAMASK_SECRET,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
+  },
 }
 
 export default config
